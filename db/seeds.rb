a51fefb8ab1eb4fa145966751ae446928e0a66f5
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2019-05-18 14:30:10 -0300

    💖  script to seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Poke::Kind.initialize
+
+Poke::Api.query_kanto_pokedex.map do |pokemon|
+  pokemon = Poke::Api.find(pokemon)
+  evolve_from = Poke::Api.query_evolves_from_species(pokemon.name)
+  if evolve_from
+    base = Pokemon.find_by(name: evolve_from)
+    pokemon.evolve_from = base
+  end
+  puts "Creating #{pokemon.name}"
+  pokemon.save
+end
