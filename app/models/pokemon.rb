b54e9be13618dd44ee2850c7740dcba12ac6d378
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2019-05-18 14:30:49 -0300

    🛠  method to create pokemon with kind

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,4 +2,11 @@ class Pokemon < ApplicationRecord
   has_and_belongs_to_many :kind
   belongs_to :evolve_from, class_name: 'Pokemon', foreign_key: 'evolve_from_id'
 
+  class << self
+    def spawn attrs
+      kind = ::Kind.where(description: attrs[:kind])
+      new(attrs.merge({kind: kind}))
+    end
+
+  end
 end
