2ad8c44b75edfab6aa834a1cd5c87138d6a85595
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2019-05-19 12:40:08 -0300

    🛠  route to list all pokemons

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'pokemons#index'
+  root to: 'pokemons#home'
+
+  scope :api do
+    get '/' => 'pokemons#index'
+  end
 end
