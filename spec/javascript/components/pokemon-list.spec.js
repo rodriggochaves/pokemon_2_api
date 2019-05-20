3658edf8fca29851040f1a8ffccf99fb4156c008
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2019-05-19 21:19:00 -0300

    ✅  test pokemon filtering

---
+++ spec/javascript/components/pokemon-list.spec.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import PokemonList from 'packs/components/pokemon-list/pokemon-list';
+
+describe("PokemonList", () => {
+  const pokemonList = [
+    { id: 1, name: 'bulbasaur', kind: "grass/poison" },
+    { id: 2, name: 'ivysaur', kind: "grass/poison" },
+    { id: 3, name: 'venosaur', kind: "grass/poison" },
+  ];
+
+  it("renders all pokemons", () => {
+    const wrapper = shallow(
+      <PokemonList
+        pokemons={pokemonList}
+        query=""
+      />
+    );
+
+    expect(wrapper.find('tbody').find('tr').length).toEqual(3);
+  });
+
+  it("can filter pokemon list", () => {
+    const wrapper = shallow(
+      <PokemonList
+        pokemons={pokemonList}
+        query="bul"
+      />
+    );
+
+    expect(wrapper.find('tbody').find('tr').length).toEqual(1);
+  });
+});
