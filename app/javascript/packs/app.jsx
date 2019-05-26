c223e839ba7e03ea5dbe25adc1a1d6baa42e794e
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2019-05-26 19:01:01 -0300

    add update page to routes

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -1,15 +1,16 @@
-import React from "react"
-import { Provider } from "react-redux"
-import thunk from "redux-thunk"
-import { createStore, applyMiddleware } from "redux"
-import { BrowserRouter, Route } from "react-router-dom"
+import React from "react";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import PokedexContainer from "./pokedex-container"
-import PokemonPageContainer from "./components/pokemon-page/pokemon-page-container"
-import CreatePokemonPageContainer from './components/create-pokemon-page/create-pokemon-page-container'
-import pokedexApp from "./reducers"
+import PokedexContainer from "./pokedex-container";
+import PokemonPageContainer from "./components/pokemon-page/pokemon-page-container";
+import CreatePageContainer from "./components/create-page/create-page-container";
+import UpdatePageContainer from "packs/components/update-page/update-page-container";
+import pokedexApp from "./reducers";
 
-const store = createStore(pokedexApp, applyMiddleware(thunk))
+const store = createStore(pokedexApp, applyMiddleware(thunk));
 
 export default props => {
   return (
@@ -17,8 +18,9 @@ export default props => {
       <BrowserRouter>
         <Route path="/" exact component={PokedexContainer} />
         <Route path="/pokemon/:id" component={PokemonPageContainer} />
-        <Route path="/create/" component={CreatePokemonPageContainer} />
+        <Route path="/create/" component={CreatePageContainer} />
+        <Route path="/pokemons/:id/update" component={UpdatePageContainer} />
       </BrowserRouter>
     </Provider>
-  )
-}
+  );
+};
