bd8ec5321a06cec8a58056048af3bef11e84846b
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2019-05-27 21:13:02 -0300

    clean code

--- app/javascript/packs/components/create-page/create-page-container.jsx
+++ app/javascript/packs/components/create-page/create-page-container.jsx
@@ -1,7 +1,7 @@
 import { connect } from "react-redux";
 
 import CreatePage from "./create-page";
-import { postPokemon, getKinds } from "packs/actions";
+import { postPokemon } from "packs/actions";
 
 const mapStateToProps = (state, ownProps) => {
   return {
