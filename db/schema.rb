19a5819b7feb0931cd94ae0027942245557e19fa
rodriggochaves <rodriggo.chaves@gmail.com>
2019-11-13 10:25:33 -0300

    🛠  rubocop ignore db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,30 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_519_200_110) do
+ActiveRecord::Schema.define(version: 2019_05_19_200110) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'kinds', force: :cascade do |t|
-    t.string 'description'
-    t.string 'color'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "kinds", force: :cascade do |t|
+    t.string "description"
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'kinds_pokemons', id: false, force: :cascade do |t|
-    t.bigint 'kind_id', null: false
-    t.bigint 'pokemon_id', null: false
-    t.index %w[kind_id pokemon_id], name: 'index_kinds_pokemons_on_kind_id_and_pokemon_id'
-    t.index %w[pokemon_id kind_id], name: 'index_kinds_pokemons_on_pokemon_id_and_kind_id'
+  create_table "kinds_pokemons", id: false, force: :cascade do |t|
+    t.bigint "kind_id", null: false
+    t.bigint "pokemon_id", null: false
+    t.index ["kind_id", "pokemon_id"], name: "index_kinds_pokemons_on_kind_id_and_pokemon_id"
+    t.index ["pokemon_id", "kind_id"], name: "index_kinds_pokemons_on_pokemon_id_and_kind_id"
   end
 
-  create_table 'pokemons', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'poke_index'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.integer 'evolve_from_id'
-    t.string 'image_url'
+  create_table "pokemons", force: :cascade do |t|
+    t.string "name"
+    t.integer "poke_index"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "evolve_from_id"
+    t.string "image_url"
   end
+
 end
